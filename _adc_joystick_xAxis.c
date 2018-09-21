9ff727478a0bfb83759b610ea27185707833dc34
Shivaakriti <42892589+Shivaakriti@users.noreply.github.com>
2018-09-21 21:13:27 +0530

    Add files via upload

---
+++ _adc_joystick_xAxis.c
@@ -0,0 +1,43 @@
+/*
+ * _adc_joystick_xAxis.c
+ *
+ * Created: 15-09-2018 07:14:39
+ *  Author: _shivaakriti
+ */ 
+
+
+
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+
+void sys_init(void);
+void port_init(void);
+void adc_init(void);
+
+int main(void)
+{
+	sys_init();
+	while(1)
+	{
+		ADCSRA|=(1<<ADSC);
+		while (!(ADCSRA&(1<<ADIF)));
+		PORTC=ADCH;
+		_delay_ms(500);
+	}
+}
+
+void sys_init(){
+	port_init();
+	adc_init();
+}
+
+void port_init(){
+	DDRC=0xff;
+	PORTC=0x00;
+}
+
+void adc_init(){
+	ADMUX=(1<<REFS0)|(1<<REFS1)|(1<<ADLAR)|(1<<MUX0);
+	ADCSRA=(1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);
+}
---
+++ _adc_ldr_interrupt.c
@@ -0,0 +1,46 @@
+/*
+ * _adc_ldr_interrupt.c
+ *
+ * Created: 16-09-2018 07:22:01
+ *  Author: _shivaakriti
+ */ 
+
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void sys_init();
+void port_init();
+void adc_init();
+
+int main(void)
+{
+	sys_init();
+	sei();
+    while(1)
+    {
+    }
+}
+
+void sys_init(){
+	port_init();
+	adc_init();
+}
+
+void port_init(){
+	DDRC=0xff;
+	PORTC=0x00;
+}
+
+void adc_init(){
+	ADMUX=(1<<REFS1)|(1<<REFS0)|(1<<ADLAR);
+	ADCSRA=(1<<ADEN)|(1<<ADSC)|(1<<ADATE)|(1<<ADIE)|(7<<ADPS0);
+}
+
+ISR(ADC_vect){
+	cli();
+	PORTC=ADCH;
+	_delay_ms(100);
+	sei();
+}
